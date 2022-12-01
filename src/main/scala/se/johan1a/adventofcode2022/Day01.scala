b2a2edfc90eb6c24c6a087c4fe73d6eb9d3f6ee7
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-01 08:45:37 +0100

    day01

--- src/main/scala/se/johan1a/adventofcode2022/Day01.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day01.scala
@@ -3,10 +3,32 @@ package se.johan1a.adventofcode2022
 object Day01 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    var i = 0
+    var elves = Seq[Int]()
+    while (i < input.size) {
+      var elf = 0
+      while (i < input.size && input(i).nonEmpty) {
+        elf += input(i).toInt
+        i += 1
+      }
+      elves = elves :+ elf
+      i += 1
+    }
+    elves.max
   }
 
   def part2(input: Seq[String]): Int = {
-    -1
+    var i = 0
+    var elves = Seq[Int]()
+    while (i < input.size) {
+      var elf = 0
+      while (i < input.size && input(i).nonEmpty) {
+        elf += input(i).toInt
+        i += 1
+      }
+      elves = elves :+ elf
+      i += 1
+    }
+    elves.sorted.reverse.take(3).sum
   }
 }
