457e9d143545e2eb9b0107ca38a9fb07765c98f0
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-01 08:50:07 +0100

    day01 refactor

--- src/main/scala/se/johan1a/adventofcode2022/Day01.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day01.scala
@@ -3,21 +3,14 @@ package se.johan1a.adventofcode2022
 object Day01 {
 
   def part1(input: Seq[String]): Int = {
-    var i = 0
-    var elves = Seq[Int]()
-    while (i < input.size) {
-      var elf = 0
-      while (i < input.size && input(i).nonEmpty) {
-        elf += input(i).toInt
-        i += 1
-      }
-      elves = elves :+ elf
-      i += 1
-    }
-    elves.max
+    parseElves(input).max
   }
 
   def part2(input: Seq[String]): Int = {
+    parseElves(input).sorted.reverse.take(3).sum
+  }
+
+  private def parseElves(input: Seq[String]): Seq[Int] = {
     var i = 0
     var elves = Seq[Int]()
     while (i < input.size) {
@@ -29,6 +22,6 @@ object Day01 {
       elves = elves :+ elf
       i += 1
     }
-    elves.sorted.reverse.take(3).sum
+    elves
   }
 }
