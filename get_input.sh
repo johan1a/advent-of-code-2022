3d7f31586f650f0170a1a9d120e71d9c2b96f209
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-01 18:55:45 +0100

    Add script for fetching input

---
+++ get_input.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/fish
+
+mkdir -p ./info
+
+set year 2022
+set day $argv
+
+set session (cat .session)
+
+if [ -z $day ] ;
+  echo 'Usage: ./get_input.sh x'
+  exit 1
+end
+
+if [ -z $session ] ;
+  echo 'Could not fetch session from .session'
+  exit 1
+end
+
+if [ $day -lt "10" ];
+  set paddedDay "0$day"
+else
+  set paddedDay $day
+end
+
+set currentDateTime (date -Is)
+set minDateTime $year-12-{$paddedDay}T06:00:00+01:00
+
+set currentDateTimeSeconds (date -d $currentDateTime +%s)
+set minDateTimeSeconds (date -d $minDateTime +%s)
+
+if [ "$currentDateTimeSeconds" -lt "$minDateTimeSeconds" ];
+  echo This puzzle is not available yet. Try again in (math $minDateTimeSeconds - $currentDateTimeSeconds) seconds.
+  exit 1
+end
+
+set existingInputFileSize (wc -l ./src/test/resources/day$paddedDay/input.txt | awk '{print $1}')
+
+if [ $existingInputFileSize -eq "0" ];
+  echo Downloading input...
+  http "https://adventofcode.com/$year/day/$day/input" cookie:session=$session > ./src/test/resources/day$paddedDay/input.txt
+  echo Done
+end
+
+if [ ! -f "./info/day$paddedDay.html" ];
+  echo Downloading info...
+  http "https://adventofcode.com/$year/day/$day" cookie:session=$session > ./info/day$paddedDay.html
+  echo Done...
+end
+
+elinks info/day$paddedDay.html
+
+echo My work is done here... Good luck, and Ho ho ho!
