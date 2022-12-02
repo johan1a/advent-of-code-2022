29625fa41223bcdc19e434a0e37bd83d3aa1219d
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-02 08:30:42 +0100

    day02

--- src/main/scala/se/johan1a/adventofcode2022/Day02.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day02.scala
@@ -2,11 +2,67 @@ package se.johan1a.adventofcode2022
 
 object Day02 {
 
+  //score 1 rock 2 paper 3 scissors
+
   def part1(input: Seq[String]): Int = {
-    -1
+    input
+      .map(line => {
+        val split = line.split(" ")
+        val them = split.head.toString
+        val me = split.last.toString
+        val shapeScore = me match {
+          case "X" => 1 // Rock
+          case "Y" => 2 // paper
+          case "Z" => 3 // scissors
+        }
+        // a rock, b paper, c scissor
+        val winScore = (me, them) match {
+          case ("X", "A") => 3
+          case ("Y", "B") => 3
+          case ("Z", "C") => 3
+          case ("X", "C") => 6
+          case ("Y", "A") => 6
+          case ("Z", "B") => 6
+          case (_, _)     => 0
+        }
+        shapeScore + winScore
+      })
+      .sum
   }
 
   def part2(input: Seq[String]): Int = {
-    -1
+    val required = Map(
+      "A" -> Map("W" -> "Y", "L" -> "Z", "D" -> "X"),
+      "B" -> Map("W" -> "Z", "L" -> "X", "D" -> "Y"),
+      "C" -> Map("W" -> "X", "L" -> "Y", "D" -> "Z")
+    )
+    input
+      .map(line => {
+        val split = line.split(" ")
+        val them = split.head.toString
+        val outcome = split.last.toString
+          .replace("X", "L")
+          .replace("Y", "D")
+          .replace("Z", "W")
+        val me = required(them)(outcome)
+        val shapeScore = me match {
+          case "X" => 1 // Rock
+          case "Y" => 2 // paper
+          case "Z" => 3 // scissors
+        }
+        // a rock, b paper, c scissor
+        val winScore = (me, them) match {
+          case ("X", "A") => 3
+          case ("Y", "B") => 3
+          case ("Z", "C") => 3
+          case ("X", "C") => 6
+          case ("Y", "A") => 6
+          case ("Z", "B") => 6
+          case (_, _)     => 0
+        }
+        shapeScore + winScore
+        shapeScore + winScore
+      })
+      .sum
   }
 }
--- src/test/scala/se/johan1a/adventofcode2022/Day02Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day02Test.scala
@@ -5,11 +5,19 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 class Day02Test extends munit.FunSuite {
 
   test("Part 1") {
-    assertEquals(Day02.part1(getInput("day02/input.txt")), -1)
+    assertEquals(Day02.part1(getInput("day02/test0.txt")), 15)
+  }
+
+  test("Part 1") {
+    assertEquals(Day02.part1(getInput("day02/input.txt")), 12276)
   }
 
   test("Part 2") {
-    assertEquals(Day02.part2(getInput("day02/input.txt")), -1)
+    assertEquals(Day02.part2(getInput("day02/test0.txt")), 12)
+  }
+
+  test("Part 2") {
+    assertEquals(Day02.part2(getInput("day02/input.txt")), 9975)
   }
 
 }
