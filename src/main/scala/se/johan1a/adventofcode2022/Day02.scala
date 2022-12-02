a7a23edb370b3250b6d98f081d19c569ea9b42e4
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-02 08:55:43 +0100

    day02 refactor

--- src/main/scala/se/johan1a/adventofcode2022/Day02.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day02.scala
@@ -2,67 +2,68 @@ package se.johan1a.adventofcode2022
 
 object Day02 {
 
-  //score 1 rock 2 paper 3 scissors
-
   def part1(input: Seq[String]): Int = {
     input
       .map(line => {
         val split = line.split(" ")
-        val them = split.head.toString
-        val me = split.last.toString
-        val shapeScore = me match {
-          case "X" => 1 // Rock
-          case "Y" => 2 // paper
-          case "Z" => 3 // scissors
-        }
-        // a rock, b paper, c scissor
-        val winScore = (me, them) match {
-          case ("X", "A") => 3
-          case ("Y", "B") => 3
-          case ("Z", "C") => 3
-          case ("X", "C") => 6
-          case ("Y", "A") => 6
-          case ("Z", "B") => 6
-          case (_, _)     => 0
-        }
-        shapeScore + winScore
+        val them = abcToMove(split.head.toString)
+        val me = xyzToMove(split.last.toString)
+        shapeScore(me) + winScore(me, them)
       })
       .sum
   }
 
   def part2(input: Seq[String]): Int = {
-    val required = Map(
-      "A" -> Map("W" -> "Y", "L" -> "Z", "D" -> "X"),
-      "B" -> Map("W" -> "Z", "L" -> "X", "D" -> "Y"),
-      "C" -> Map("W" -> "X", "L" -> "Y", "D" -> "Z")
+    val requiredMove = Map(
+      "R" -> Map("W" -> "P", "L" -> "S", "D" -> "R"),
+      "P" -> Map("W" -> "S", "L" -> "R", "D" -> "P"),
+      "S" -> Map("W" -> "R", "L" -> "P", "D" -> "S")
     )
+
     input
       .map(line => {
         val split = line.split(" ")
-        val them = split.head.toString
-        val outcome = split.last.toString
-          .replace("X", "L")
-          .replace("Y", "D")
-          .replace("Z", "W")
-        val me = required(them)(outcome)
-        val shapeScore = me match {
-          case "X" => 1 // Rock
-          case "Y" => 2 // paper
-          case "Z" => 3 // scissors
-        }
-        // a rock, b paper, c scissor
-        val winScore = (me, them) match {
-          case ("X", "A") => 3
-          case ("Y", "B") => 3
-          case ("Z", "C") => 3
-          case ("X", "C") => 6
-          case ("Y", "A") => 6
-          case ("Z", "B") => 6
-          case (_, _)     => 0
-        }
-        shapeScore + winScore
-        shapeScore + winScore
+
+        val them = abcToMove(split.head.toString)
+        val outcome = xyzToOutcome(split.last.toString)
+        val me = requiredMove(them)(outcome)
+
+        shapeScore(me) + winScore(me, them)
       })
       .sum
+  }
+
+  private def xyzToMove: String => String = {
+    case "X" => "R"
+    case "Y" => "P"
+    case "Z" => "S"
+  }
+
+  private def xyzToOutcome: String => String = {
+    case "X" => "L"
+    case "Y" => "D"
+    case "Z" => "W"
+  }
+
+  private def abcToMove: String => String = {
+    case "A" => "R"
+    case "B" => "P"
+    case "C" => "S"
+  }
+
+  private def shapeScore: String => Int = {
+    case "R" => 1
+    case "P" => 2
+    case "S" => 3
+  }
+
+  private def winScore: (String, String) => Int = {
+    case ("R", "R") => 3
+    case ("P", "P") => 3
+    case ("S", "S") => 3
+    case ("R", "S") => 6
+    case ("P", "R") => 6
+    case ("S", "P") => 6
+    case (_, _)     => 0
   }
 }
