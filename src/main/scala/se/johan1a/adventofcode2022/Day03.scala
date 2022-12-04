881eb5db705fbddb2e0cde7685092e8a3f59289a
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-04 11:39:44 +0100

    day 03 part 1

--- src/main/scala/se/johan1a/adventofcode2022/Day03.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day03.scala
@@ -3,7 +3,23 @@ package se.johan1a.adventofcode2022
 object Day03 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    input.map(line => {
+      val a = line.take(line.size / 2).toSet
+      val b = line.drop(line.size / 2).toSet
+      val common = a.intersect(b).head.toString
+      println(common)
+      priority(common)
+    }).sum
+  }
+
+  private def priority(l: String): Int = {
+    if (l >= "a" && l <= "z") {
+      l.toCharArray().head.toInt - 'a'.toInt + 1
+    } else if (l >= "A" && l <= "Z") {
+      l.toCharArray().head.toInt - 'A'.toInt + 27
+    } else {
+      ???
+    }
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2022/Day03Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day03Test.scala
@@ -4,12 +4,16 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 
 class Day03Test extends munit.FunSuite {
 
+  test("Part 1 test") {
+    assertEquals(Day03.part1(getInput("day03/test0.txt")), 157)
+  }
+
   test("Part 1") {
     assertEquals(Day03.part1(getInput("day03/input.txt")), -1)
   }
 
-  test("Part 2") {
-    assertEquals(Day03.part2(getInput("day03/input.txt")), -1)
-  }
+  // test("Part 2") {
+  //   assertEquals(Day03.part2(getInput("day03/input.txt")), -1)
+  // }
 
 }
