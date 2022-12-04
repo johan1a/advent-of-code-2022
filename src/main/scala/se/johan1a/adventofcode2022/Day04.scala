e32d2d1a88298ecd07904eb12f82078ce9b3ccc2
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-04 11:24:13 +0100

    day 04 part 1

--- src/main/scala/se/johan1a/adventofcode2022/Day04.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day04.scala
@@ -1,9 +1,24 @@
 package se.johan1a.adventofcode2022
 
+import Utils._
+
 object Day04 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    val res = input
+      .filter(line => {
+        val split = line.split(",")
+        val a = split.head.split("-").map(_.toLong)
+        val b = split.last.split("-").map(_.toLong)
+        (a.head >= b.head && a.last <= b.last) || (b.head >= a.head && b.last <= a.last)
+      })
+      .map(x => {
+        x
+      })
+
+    res.foreach(println)
+
+    res.size
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2022/Day04Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day04Test.scala
@@ -4,12 +4,18 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 
 class Day04Test extends munit.FunSuite {
 
+  // test("Part 1") {
+  //   assertEquals(Day04.part1(getInput("day04/test0.txt")), 2)
+  // }
+
   test("Part 1") {
+    // not 630
+    // 456 too low
     assertEquals(Day04.part1(getInput("day04/input.txt")), -1)
   }
 
-  test("Part 2") {
-    assertEquals(Day04.part2(getInput("day04/input.txt")), -1)
-  }
+  // test("Part 2") {
+  //   assertEquals(Day04.part2(getInput("day04/input.txt")), -1)
+  // }
 
 }
