cefb8fd5283dd4c7697847f2f879a2cc62eb294c
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-04 11:29:43 +0100

    day 04 part 2

--- src/main/scala/se/johan1a/adventofcode2022/Day04.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day04.scala
@@ -16,12 +16,21 @@ object Day04 {
         x
       })
 
-    res.foreach(println)
-
     res.size
   }
 
   def part2(input: Seq[String]): Int = {
-    -1
+    input
+      .filter(line => {
+        val split = line.split(",")
+        val a = split.head.split("-").map(_.toLong)
+        val b = split.last.split("-").map(_.toLong)
+        val overlap = !(a.head > b.last || a.last < b.head || b.head > a.last || b.last < a.head)
+        if(overlap){
+          println(s"a: $a, b: $b")
+        }
+
+        overlap
+      }).size
   }
 }
