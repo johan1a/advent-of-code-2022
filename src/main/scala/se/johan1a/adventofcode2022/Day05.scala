cdd5fca42be8bea3fbee759a6e9e0844e2bc8080
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-05 08:44:58 +0100

    Day 05 part 1

--- src/main/scala/se/johan1a/adventofcode2022/Day05.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day05.scala
@@ -1,9 +1,44 @@
 package se.johan1a.adventofcode2022
 
+import Utils._
+
 object Day05 {
 
-  def part1(input: Seq[String]): Int = {
-    -1
+  def part1(input: Seq[String]): String = {
+    var i = 0
+    var stacks: Map[Int, Seq[Char]] = Map()
+    while (!input(i).startsWith(" 1")) {
+      var j = 0
+      val line = input(i)
+
+      while (j < input(i).size) {
+        if (line.charAt(j) == '[') {
+          val index = j / 4
+          val stack = stacks.getOrElse(index, Seq())
+          stacks = stacks.updated(index, line.charAt(j + 1) +: stack)
+        }
+
+        j += 1
+      }
+
+      i += 1
+    }
+    i += 2
+    println(stacks)
+    val instructions = input.drop(i).map(numbers)
+    instructions.foreach { case (instruction) => {
+      val (n, src, dest) = (instruction(0).toInt, instruction(1).toInt - 1, instruction(2).toInt - 1)
+      val sourceStack = stacks(src)
+      val top = sourceStack.drop(sourceStack.size - n)
+      stacks = stacks.updated(src, stacks(src).take(sourceStack.size - n))
+
+      val destStack = stacks(dest)
+      stacks = stacks.updated(dest, destStack ++ top.reverse)
+      println(s"$instruction, $stacks")
+    } }
+    stacks.keys.toSeq.sorted.map(key => {
+      stacks(key).last
+    }).mkString
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2022/Day05Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day05Test.scala
@@ -4,12 +4,16 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 
 class Day05Test extends munit.FunSuite {
 
-  test("Part 1") {
-    assertEquals(Day05.part1(getInput("day05/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day05.part1(getInput("day05/test0.txt")), "CMZ")
   }
 
-  test("Part 2") {
-    assertEquals(Day05.part2(getInput("day05/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day05.part1(getInput("day05/input.txt")), "")
   }
+
+//   test("Part 2") {
+//     assertEquals(Day05.part2(getInput("day05/input.txt")), -1)
+//   }
 
 }
