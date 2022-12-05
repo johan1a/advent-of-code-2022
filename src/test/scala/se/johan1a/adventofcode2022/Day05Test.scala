17b0297afced8b9f323d69aea39cefaaa8531766
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-05 08:47:25 +0100

    Day 05 part 2

--- src/test/scala/se/johan1a/adventofcode2022/Day05Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day05Test.scala
@@ -9,11 +9,15 @@ class Day05Test extends munit.FunSuite {
   }
 
   test("Part 1") {
-    assertEquals(Day05.part1(getInput("day05/input.txt")), "")
+    assertEquals(Day05.part1(getInput("day05/input.txt")), "ZRLJGSCTR")
   }
 
-//   test("Part 2") {
-//     assertEquals(Day05.part2(getInput("day05/input.txt")), -1)
-//   }
+  test("Part 2 test") {
+    assertEquals(Day05.part2(getInput("day05/test0.txt")), "MCD")
+  }
+
+  test("Part 2 test") {
+    assertEquals(Day05.part2(getInput("day05/input.txt")), "")
+  }
 
 }
