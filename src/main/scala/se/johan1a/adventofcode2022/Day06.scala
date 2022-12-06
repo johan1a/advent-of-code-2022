68a1739c326214b234a342d48f49488684802d4c
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-06 08:19:09 +0100

    refactor

--- src/main/scala/se/johan1a/adventofcode2022/Day06.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day06.scala
@@ -2,21 +2,17 @@ package se.johan1a.adventofcode2022
 
 object Day06 {
 
-  def part1(input: Seq[String]): Int = {
-    val line = input.head
+  def part1(input: String): Int = {
     var i = 4
-    println(line.substring(i-4,i).toSet)
-    while (i < line.size && line.substring(i-4,i).toSet.size != 4) {
+    while (i < input.size && input.substring(i - 4, i).toSet.size != 4) {
       i += 1
     }
     i
   }
 
-  def part2(input: Seq[String]): Int = {
-    val line = input.head
+  def part2(input: String): Int = {
     var i = 14
-    println(line.substring(i-14,i).toSet)
-    while (i < line.size && line.substring(i-14,i).toSet.size != 14) {
+    while (i < input.size && input.substring(i - 14, i).toSet.size != 14) {
       i += 1
     }
     i
--- src/test/scala/se/johan1a/adventofcode2022/Day06Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day06Test.scala
@@ -5,18 +5,18 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 class Day06Test extends munit.FunSuite {
 
   test("Part 1") {
-    assertEquals(Day06.part1(getInput("day06/test0.txt")), 7)
+    assertEquals(Day06.part1(getInput("day06/test0.txt").head), 7)
   }
 
   test("Part 1") {
-    assertEquals(Day06.part1(getInput("day06/input.txt")), 1356)
+    assertEquals(Day06.part1(getInput("day06/input.txt").head), 1356)
   }
 
   test("Part 2 test") {
-    assertEquals(Day06.part2(getInput("day06/test0.txt")), 19)
+    assertEquals(Day06.part2(getInput("day06/test0.txt").head), 19)
   }
 
   test("Part 2") {
-    assertEquals(Day06.part2(getInput("day06/input.txt")), 2564)
+    assertEquals(Day06.part2(getInput("day06/input.txt").head), 2564)
   }
 }
