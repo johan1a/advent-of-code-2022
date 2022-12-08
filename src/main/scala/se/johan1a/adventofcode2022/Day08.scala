6188db531dfcff44b8abb6ca8b193984dc6f783b
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-08 09:51:50 +0100

    day08 part 2

--- src/main/scala/se/johan1a/adventofcode2022/Day08.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day08.scala
@@ -64,13 +64,79 @@ object Day08 {
       }
     }
 
-    seen.foreach(println)
     val borderSize = 2 * xSize + 2 * ySize - 4
-    println(borderSize)
     seen.size + borderSize
   }
 
   def part2(input: Seq[String]): Int = {
-    -1
+    val xSize = input.head.size
+    val ySize = input.size
+    var best = -1
+
+    0.until(ySize).foreach { y =>
+      0.until(xSize).foreach { x =>
+        var height = input(y).charAt(x)
+        var product = 1
+
+        var y1 = y
+        var x1 = x + 1
+        var size = 0
+        while (x1 < xSize) {
+          size += 1
+          if (input(y1).charAt(x1) >= height) {
+            x1 = xSize
+          }
+          x1 += 1
+        }
+        println(size)
+        product = product * size
+
+        y1 = y
+        x1 = x - 1
+        size = 0
+        while (x1 >= 0) {
+          size += 1
+          if (input(y1).charAt(x1) >= height) {
+            x1 = -1
+          }
+          x1 -= 1
+        }
+        println(size)
+        product = product * size
+
+        y1 = y + 1
+        x1 = x
+        size = 0
+        while (y1 < ySize) {
+          size += 1
+          if (input(y1).charAt(x1) >= height) {
+            y1 = ySize
+          }
+          y1 += 1
+        }
+        println(size)
+        product = product * size
+
+        y1 = y - 1
+        x1 = x
+        size = 0
+        while (y1 >= 0) {
+          size += 1
+          if (input(y1).charAt(x1) >= height) {
+            y1 = -1
+          }
+          y1 -= 1
+        }
+        println(size)
+        product = product * size
+
+        println(s"x: $x, y: $y, product: $product")
+        if (product > best) {
+          best = product
+        }
+      }
+    }
+
+    best
   }
 }
--- src/test/scala/se/johan1a/adventofcode2022/Day08Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day08Test.scala
@@ -12,8 +12,12 @@ class Day08Test extends munit.FunSuite {
     assertEquals(Day08.part1(getInput("day08/input.txt")), 1708)
   }
 
+   test("Part 2 test") {
+     assertEquals(Day08.part2(getInput("day08/test0.txt")), 8)
+   }
+
   test("Part 2") {
-    assertEquals(Day08.part2(getInput("day08/input.txt")), -1)
+    assertEquals(Day08.part2(getInput("day08/input.txt")), 504000)
   }
 
 }
