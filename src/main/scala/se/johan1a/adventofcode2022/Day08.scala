ea1dc201f5432bd91abd1acae38d9be9a7ddfe8c
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-08 09:31:39 +0100

    day08 part 1

--- src/main/scala/se/johan1a/adventofcode2022/Day08.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day08.scala
@@ -3,7 +3,71 @@ package se.johan1a.adventofcode2022
 object Day08 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    val xSize = input.head.size
+    val ySize = input.size
+
+    var seen = Set[(Int, Int)]()
+
+    1.until(ySize - 1).map { y =>
+      var highest = input(y).charAt(0)
+      1.until(xSize - 1).map { x =>
+        val pos = (x, y)
+        val height = input(y).charAt(x)
+        if (height > highest) {
+          highest = height
+          if (!seen.contains(pos)) {
+            seen = seen + pos
+          }
+        }
+      }
+    }
+
+    (1).until(ySize - 1).foreach { y =>
+      var highest = input(y).charAt(xSize - 1)
+      (1).until(xSize - 1).reverse.foreach { x =>
+        val pos = (x, y)
+        val height = input(y).charAt(x)
+        if (height > highest) {
+          highest = height
+          if (!seen.contains(pos)) {
+            seen = seen + pos
+          }
+        }
+      }
+    }
+
+    (1).until(xSize - 1).foreach { x =>
+      var highest = input(0).charAt(x)
+      (1).until(ySize - 1).foreach { y =>
+        val pos = (x, y)
+        val height = input(y).charAt(x)
+        if (height > highest) {
+          highest = height
+          if (!seen.contains(pos)) {
+            seen = seen + pos
+          }
+        }
+      }
+    }
+
+    (1).until(xSize - 1).foreach { x =>
+      var highest = input(ySize - 1).charAt(x)
+      (1).until(ySize - 1).reverse.foreach { y =>
+        val pos = (x, y)
+        val height = input(y).charAt(x)
+        if (height > highest) {
+          highest = height
+          if (!seen.contains(pos)) {
+            seen = seen + pos
+          }
+        }
+      }
+    }
+
+    seen.foreach(println)
+    val borderSize = 2 * xSize + 2 * ySize - 4
+    println(borderSize)
+    seen.size + borderSize
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2022/Day08Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day08Test.scala
@@ -5,7 +5,11 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 class Day08Test extends munit.FunSuite {
 
   test("Part 1") {
-    assertEquals(Day08.part1(getInput("day08/input.txt")), -1)
+    assertEquals(Day08.part1(getInput("day08/test0.txt")), 21)
+  }
+
+  test("Part 1") {
+    assertEquals(Day08.part1(getInput("day08/input.txt")), 1708)
   }
 
   test("Part 2") {
