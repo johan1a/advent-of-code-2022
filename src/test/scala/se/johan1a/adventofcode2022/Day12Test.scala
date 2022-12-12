ddd3ad38695c66f16cae01fc55be5710cfb8d9f2
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-12 16:58:02 +0100

    Day 12 part 1

--- src/test/scala/se/johan1a/adventofcode2022/Day12Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day12Test.scala
@@ -4,8 +4,12 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 
 class Day12Test extends munit.FunSuite {
 
+  test("Part 1 test") {
+    assertEquals(Day12.part1(getInput("day12/test0.txt")), 31)
+  }
+
   test("Part 1") {
-    assertEquals(Day12.part1(getInput("day12/input.txt")), -1)
+    assertEquals(Day12.part1(getInput("day12/input.txt")), 383)
   }
 
   test("Part 2") {
