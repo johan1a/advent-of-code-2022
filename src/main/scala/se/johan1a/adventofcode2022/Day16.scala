225bcecd7a9ec963aab25634f9b942978b22b11e
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-16 22:47:26 +0100

    day 16 part 1 faster

--- src/main/scala/se/johan1a/adventofcode2022/Day16.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day16.scala
@@ -1,6 +1,7 @@
 package se.johan1a.adventofcode2022
 
 import scala.collection.mutable.Queue
+import scala.collection.mutable
 
 object Day16 {
 
@@ -10,34 +11,23 @@ object Day16 {
       var next: Seq[(String, Int)],
       open: Boolean
   )
-
-  case class State(
-      valves: Map[String, Boolean],
-      current: String,
-      total: Short,
-      minutesLeft: Short
-  )
-
-  var cache = Map[State, (Short, Seq[String])]()
   var valves = Map[String, Valve]()
-  var i = 0
 
   def part1(input: Seq[String]): Int = {
-    cache = Map()
-    i = 0
-    valves = reduceGraph(
+    val (valves0, dist0) = reduceGraph2(
       input.map(parse).map { v => v.name -> v }.toMap
     )
+    dist = dist0
+    valves2 = valves0
+
     val start = "AA"
-    println(s"reduced valves:")
-    valves.foreach(println)
-    val maxPossibleFlow = valves.map(_._2.rate).sum.toShort
-    val initialStates: Map[String, Boolean] =
-      valves.map(e => e._1 -> false).toMap
-    val (value, path) =
-      findBest(initialStates, maxPossibleFlow, start, 0, 0, 30)
-    println(path)
-    value
+
+    val leftToVisit = valves2.map(_._1).filterNot(_ == start).toSeq
+
+    val results = findBest2(leftToVisit, start, 30)
+    val rr = results.sortBy(_._1)
+
+    rr.last._1
   }
 
   private def reduceGraph(valves: Map[String, Valve]): Map[String, Valve] = {
@@ -66,72 +56,90 @@ object Day16 {
     reducedValves
   }
 
-  private def findBest(
-      valveStates: Map[String, Boolean],
-      maxPossibleFlow: Short,
-      current: String,
-      total: Short,
-      flow: Short,
-      minutesLeft: Short
-  ): (Short, Seq[String]) = {
+  var valves2 = Map[String, Valve]()
+  var dist = mutable.Map[(String, String), Short]()
 
-    val state = State(valveStates, current, total, minutesLeft)
-    val cached = cache.get(state)
-    cached match {
-      case Some(value) => value
-      case None =>
-        val result: (Short, Seq[String]) = if (minutesLeft == 0) {
-          (total, Seq(current))
-        } else if (flow == maxPossibleFlow) {
-          (total, Seq(current))
-        } else {
-          i += 1
-          if (i % 100000 == 0) {
-            println(i)
-          }
-          if (i > 20000000) {
-            throw new Exception(s"Too many iters, minutes left: $minutesLeft")
-          }
-          val valve = valves(current)
-          lazy val openResult = findBest(
-            valveStates + (current -> true),
-            maxPossibleFlow,
-            current,
-            (total + valve.rate * (minutesLeft - 1)).toShort,
-            (flow + valve.rate).toShort,
-            (minutesLeft - 1).toShort
+  private def findBest2(
+      leftToVisit: Seq[String],
+      current: String,
+      minutesLeft: Short
+  ): Seq[(Short, Seq[String])] = {
+
+    val currentValve = valves2(current)
+    val valveTotal = currentValve.rate * minutesLeft
+
+    val r = leftToVisit
+      .filter(next => dist((current, next)) + 1 <= minutesLeft)
+      .flatMap { next =>
+        val d = dist((current, next))
+        val results =
+          findBest2(
+            leftToVisit.filterNot(_ == next),
+            next,
+            (minutesLeft - d - 1).toShort
           )
 
-          lazy val moveResults =
-            valve.next.filter(_._2 <= minutesLeft).map { case (next, dist) =>
-              findBest(
-                valveStates,
-                maxPossibleFlow,
-                next,
-                total,
-                flow,
-                (minutesLeft - dist).toShort
-              )
-            }
-          if (valve.rate > 0 && !valveStates(current)) {
-            val (v, p) = (openResult +: moveResults).maxBy(_._1)
-            (v, (current +: p))
-          } else if (moveResults.nonEmpty) {
-            val (v, p) = moveResults.maxBy(_._1)
-            (v, (current +: p))
-          } else {
-            (total, Seq(current))
+        val r0 = results.map { case (value, path) =>
+          ((value + valveTotal).toShort, next +: path)
+        }
+        r0
+      }
+
+    if (r.nonEmpty) {
+      r
+    } else {
+      Seq((valveTotal.toShort, Seq(current)))
+    }
+  }
+
+  private def reduceGraph2(
+      valves: Map[String, Valve]
+  ): (Map[String, Valve], mutable.Map[(String, String), Short]) = {
+    var reducedValves = valves
+
+    valves.values.foreach { valve0 =>
+      if (valve0.rate == 0 && valve0.name != "AA") {
+        val neighbors = valve0.next
+        valves.values.foreach { valve1 =>
+          valve1.next.find(_._1 == valve0.name) match {
+            case Some((n0, d0)) =>
+              valve1.next =
+                valve1.next.filterNot(_._1 == valve0.name) ++ neighbors
+                  .map { case (n1, d1) =>
+                    (n1, d1 + d0)
+                  }
+                  .filterNot(_._1 == valve1.name)
+            case None =>
           }
         }
 
-        cache = cache + (state -> result)
-        result
+        reducedValves = reducedValves.removed(valve0.name)
+      }
     }
 
-  }
+    var dist =
+      mutable.Map[(String, String), Short]().withDefaultValue(10000.toShort)
 
-  def part2(input: Seq[String]): Int = {
-    -1
+    reducedValves.values.foreach { valve =>
+      dist((valve.name, valve.name)) = 0
+      valve.next.foreach { case (neighbor, neighborDist) =>
+        dist((valve.name, neighbor)) = neighborDist.toShort
+      }
+    }
+
+    reducedValves.values.foreach { k =>
+      reducedValves.values.foreach { i =>
+        reducedValves.values.foreach { j =>
+          val potential =
+            (dist((i.name, k.name)) + dist((k.name, j.name))).toShort
+          if (dist((i.name, j.name)) > potential) {
+            dist((i.name, j.name)) = potential
+          }
+        }
+      }
+    }
+
+    (reducedValves, dist)
   }
 
   private def parse(line: String) = {
--- src/test/scala/se/johan1a/adventofcode2022/Day16Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day16Test.scala
@@ -4,17 +4,16 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 
 class Day16Test extends munit.FunSuite {
 
-  test("Part 1") {
+  test("Part 1 test") {
     assertEquals(Day16.part1(getInput("day16/test0.txt")), 1651)
   }
 
-  // requires sbt -mem 3096
   test("Part 1") {
     assertEquals(Day16.part1(getInput("day16/input.txt")), 1584)
   }
 
-  test("Part 2") {
-    assertEquals(Day16.part2(getInput("day16/input.txt")), -1)
-  }
+  // test("Part 2") {
+  //   assertEquals(Day16.part2(getInput("day16/test0.txt")), 1707)
+  // }
 
 }
