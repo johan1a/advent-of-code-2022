f2cc405f431a9a291067b75e3617d15ebaac0ab5
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-16 16:26:02 +0100

    day 16 part 1, sbt -mem 14096

--- src/test/scala/se/johan1a/adventofcode2022/Day16Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day16Test.scala
@@ -9,7 +9,7 @@ class Day16Test extends munit.FunSuite {
   }
 
   test("Part 1") {
-    assertEquals(Day16.part1(getInput("day16/input.txt")), 1)
+    assertEquals(Day16.part1(getInput("day16/input.txt")), 1584)
   }
 
   test("Part 2") {
