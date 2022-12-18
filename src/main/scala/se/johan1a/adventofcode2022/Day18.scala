572b2cb6b070458ab3c83fd76c9b24bbad407936
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-18 12:55:20 +0100

    day18 part 2

--- src/main/scala/se/johan1a/adventofcode2022/Day18.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day18.scala
@@ -1,13 +1,14 @@
 package se.johan1a.adventofcode2022
 
 import Utils._
+import scala.collection.mutable.Queue
 
 object Day18 {
 
   def part1(input: Seq[String]): Int = {
     val cubes = parse(input)
     var sum = 0
-    val r1 = cubes
+    cubes
       .foreach {
         case (cube: Vec3) => {
           val nn = neighbors3(cube)
@@ -28,8 +29,82 @@ object Day18 {
     sum
   }
 
+  val maxPocketSize = 1100
+
   def part2(input: Seq[String]): Int = {
-    -1
+    val cubes = parse(input)
+    var pockets = Set[Vec3]()
+    var air = Set[Vec3]()
+    var sum = 0
+    var i = 0
+    cubes
+      .foreach {
+        case (cube: Vec3) => {
+          i += 1
+          println(s"checking cube $i of ${cubes.size} air size: ${air.size}, pocket size: ${pockets.size}")
+          val nn = neighbors3(cube)
+          assert(nn.size == 6)
+          val r = nn
+            .map(neighbor => {
+              if (cubes.contains(neighbor)) {
+                // println(s"not counting $neighbor - in cubes")
+                0
+              } else if (pockets.contains(neighbor)) {
+                // println(s"not counting $neighbor - in pocket")
+                0
+              } else if (air.contains(neighbor)) {
+                1
+              } else {
+                // println(s"flood at $neighbor")
+                val pocket = flood(cubes, air, neighbor)
+                pocket match {
+                  case Pocket(cc) =>
+                    pockets = pockets ++ cc
+                    0
+                  case Air(cc) =>
+                    air = air ++ cc
+                    1
+                }
+              }
+            })
+            .sum
+          sum += r
+        }
+      }
+
+    sum
+  }
+
+  sealed trait PocketT
+  case class Pocket(cubes: Set[Vec3]) extends PocketT
+  case class Air(cubes: Set[Vec3]) extends PocketT
+
+  def flood(cubes: Set[Vec3], air: Set[Vec3], pos: Vec3): PocketT = {
+    var pocket = Set[Vec3]()
+    val queue = Queue[Vec3](pos)
+
+    while (queue.nonEmpty && pocket.size < maxPocketSize) {
+      if(pocket.size %  100 == 0){
+        println(s"queue size: ${queue.size}, pocket size: ${pocket.size}")
+      }
+      var current = queue.dequeue()
+
+      if (!cubes.contains(current)) {
+        pocket = pocket + current
+      }
+      if (air.contains(current)) {
+        return Air(pocket)
+      }
+      queue.enqueueAll(neighbors3(current).filterNot(c => {
+        pocket.contains(c) || cubes.contains(c)
+      }))
+    }
+
+    if (pocket.size >= maxPocketSize) {
+      Air(pocket)
+    } else {
+      Pocket(pocket)
+    }
   }
 
   def parse(input: Seq[String]): Set[Vec3] = {
--- src/test/scala/se/johan1a/adventofcode2022/Day18Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day18Test.scala
@@ -4,20 +4,32 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 
 class Day18Test extends munit.FunSuite {
 
-  test("Part 1") {
+  test("Part 1 test") {
     assertEquals(Day18.part1(getInput("day18/test1.txt")), 10)
   }
 
-  test("Part 1") {
+  test("Part 1 test") {
     assertEquals(Day18.part1(getInput("day18/test0.txt")), 64)
+  }
+
+  test("Part 1 test") {
+    assertEquals(Day18.part1(getInput("day18/test2.txt")), 76)
   }
 
   test("Part 1") {
     assertEquals(Day18.part1(getInput("day18/input.txt")), 3396)
   }
 
+  test("Part 2 test") {
+    assertEquals(Day18.part2(getInput("day18/test0.txt")), 58)
+  }
+
+  test("Part 2 test") {
+    assertEquals(Day18.part2(getInput("day18/test2.txt")), 66)
+  }
+
   test("Part 2") {
-    assertEquals(Day18.part2(getInput("day18/input.txt")), -1)
+    assertEquals(Day18.part2(getInput("day18/input.txt")), 2044)
   }
 
 }
