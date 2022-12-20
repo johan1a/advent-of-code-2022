00beb4dd19ca53db5858267e8163e6cc420e7a8e
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-20 20:32:33 +0100

    day 20 refactor

--- src/main/scala/se/johan1a/adventofcode2022/Day20.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day20.scala
@@ -10,72 +10,30 @@ object Day20 {
   }
 
   def part1(input: Seq[String]): Long = {
-    var first: Node = null
-    var prev: Node = null
-    val nodes = input
-      .map(_.toInt)
-      .map { n =>
-        val node = Node(n, prev = prev, next = null)
-        if (first == null) {
-          first = node
-        }
-        if (prev != null) {
-          prev.next = node
-        }
-
-        prev = node
-        node
-      }
-      .toArray
-    first.prev = prev
-    prev.next = first
-
-    var i = 0
-    while (i < input.size) {
-      val node = nodes(i)
-      move(node, node.n % (nodes.size - 1))
-      i += 1
-    }
-    nodesToString(first)
-    val zero = nodes.find(_.n == 0).get
-    val a = find(zero, 1000)
-    val b = find(a, 1000)
-    val c = find(b, 1000)
-
-    a.n + b.n + c.n
+    val nodes = parse(input)
+    mix(nodes)
+    coordinateSum(nodes)
   }
 
   def part2(input: Seq[String]): Long = {
     val decryptionKey = 811589153L
-    var first: Node = null
-    var prev: Node = null
-    val nodes = input
-      .map(_.toInt)
-      .map { n =>
-        val node = Node(n * decryptionKey, prev = prev, next = null)
-        if (first == null) {
-          first = node
-        }
-        if (prev != null) {
-          prev.next = node
-        }
-
-        prev = node
-        node
-      }
-      .toArray
-    first.prev = prev
-    prev.next = first
-
+    val nodes = parse(input, decryptionKey)
     0.until(10).foreach { _ =>
-      var i = 0
-      while (i < input.size) {
-        val node = nodes(i)
-        move(node, node.n % (nodes.size - 1))
-        i += 1
-      }
+      mix(nodes)
     }
+    coordinateSum(nodes)
+  }
 
+  private def mix(nodes: Array[Node]): Unit = {
+    var i = 0
+    while (i < nodes.size) {
+      val node = nodes(i)
+      move(node, node.n % (nodes.size - 1))
+      i += 1
+    }
+  }
+
+  private def coordinateSum(nodes: Array[Node]) = {
     val zero = nodes.find(_.n == 0).get
     val a = find(zero, 1000)
     val b = find(a, 1000)
@@ -83,6 +41,7 @@ object Day20 {
 
     a.n + b.n + c.n
   }
+
 
   private def move(node: Node, steps: Long) = {
     if (steps > 0) {
@@ -115,9 +74,7 @@ object Day20 {
         secondPrev.next = node
         i += 1
       }
-
     }
-
   }
 
   private def find(node: Node, steps: Long) = {
@@ -138,16 +95,27 @@ object Day20 {
     next
   }
 
-  def nodesToString(start: Node) = {
-    var s = ""
-    var node = start.next
-    s += (s"${start.n}, ")
-    while (node.n != start.n) {
-      s += (node.n)
-      s += (", ")
-      node = node.next
-    }
-    s += "\n\n"
-    s
+  private def parse(input: Seq[String], factor: Long = 1) = {
+    var first: Node = null
+    var prev: Node = null
+    val nodes = input
+      .map(_.toInt)
+      .map { n =>
+        val node = Node(n * factor, prev = prev, next = null)
+        if (first == null) {
+          first = node
+        }
+        if (prev != null) {
+          prev.next = node
+        }
+
+        prev = node
+        node
+      }
+      .toArray
+    first.prev = prev
+    prev.next = first
+    nodes
   }
+
 }
