024e39bcecf71e2e9b568f7b083796a161ecdffc
Johan Andersson <johan.peter.andersson@gmail.com>
2022-12-21 10:43:09 +0100

    day 21 part 1

--- src/main/scala/se/johan1a/adventofcode2022/Day21.scala
+++ src/main/scala/se/johan1a/adventofcode2022/Day21.scala
@@ -2,11 +2,39 @@ package se.johan1a.adventofcode2022
 
 object Day21 {
 
-  def part1(input: Seq[String]): Int = {
+  sealed trait Op
+  case class Add(a: String, b: String) extends Op
+  case class Sub(a: String, b: String) extends Op
+  case class Mul(a: String, b: String) extends Op
+  case class Div(a: String, b: String) extends Op
+  case class Num(value: Long) extends Op
+
+  def part1(input: Seq[String]): Long = {
+    val instructions = input.map(parse).toMap
+    eval(instructions, "root")
+  }
+
+  def part2(input: Seq[String]): Long = {
     -1
   }
 
-  def part2(input: Seq[String]): Int = {
-    -1
+  private def eval(instructions: Map[String, Op], id: String): Long = {
+    instructions(id) match {
+      case Num(value) => value
+      case Add(a, b)  => eval(instructions, a) + eval(instructions, b)
+      case Sub(a, b)  => eval(instructions, a) - eval(instructions, b)
+      case Mul(a, b)  => eval(instructions, a) * eval(instructions, b)
+      case Div(a, b)  => eval(instructions, a) / eval(instructions, b)
+    }
+  }
+
+  private def parse(line: String): (String, Op) = {
+    line match {
+      case s"$id: $a + $b" => id -> Add(a, b)
+      case s"$id: $a - $b" => id -> Sub(a, b)
+      case s"$id: $a * $b" => id -> Mul(a, b)
+      case s"$id: $a / $b" => id -> Div(a, b)
+      case s"$id: $n"      => id -> Num(n.toLong)
+    }
   }
 }
--- src/test/scala/se/johan1a/adventofcode2022/Day21Test.scala
+++ src/test/scala/se/johan1a/adventofcode2022/Day21Test.scala
@@ -5,11 +5,15 @@ import se.johan1a.adventofcode2022.TestInputUtil.getInput
 class Day21Test extends munit.FunSuite {
 
   test("Part 1") {
-    assertEquals(Day21.part1(getInput("day21/input.txt")), -1)
+    assertEquals(Day21.part1(getInput("day21/test0.txt")), 152L)
+  }
+
+  test("Part 1") {
+    assertEquals(Day21.part1(getInput("day21/input.txt")), 56490240862410L)
   }
 
   test("Part 2") {
-    assertEquals(Day21.part2(getInput("day21/input.txt")), -1)
+    assertEquals(Day21.part2(getInput("day21/input.txt")), -1L)
   }
 
 }
